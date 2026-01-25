5d8446d1c3acca865bc25dcb1b125e5edece9b40
Igor Semyonov <igor@semyonov.xyz>
2026-01-24 22:35:55 -0500

    turned off wireguard for leto

--- hosts/leto/default.nix
+++ hosts/leto/default.nix
@@ -46,25 +46,25 @@
   networking = {
     firewall = {
     };
-    wg-quick = {
-      interfaces = {
-        fidler = {
-          autostart = true;
-          address = ["10.0.0.10/32"];
-          listenPort = 51820;
-          privateKeyFile = "/etc/wireguard/privatekey";
-          # postUp = "wg set %i private-key /etc/wireguard/privatekey";
-          peers = [
-            {
-              publicKey = "yPTvlsTZnzAxfn2GxrvSQX5/ymcsSFqSLtHiJ7zJITc=";
-              allowedIPs = ["10.0.0.0/24"];
-              endpoint = "nalgor.net:41883";
-              persistentKeepalive = 25;
-            }
-          ];
-        };
-      };
-    };
+    # wg-quick = {
+    #   interfaces = {
+    #     fidler = {
+    #       autostart = true;
+    #       address = ["10.0.0.10/32"];
+    #       listenPort = 51820;
+    #       privateKeyFile = "/etc/wireguard/privatekey";
+    #       # postUp = "wg set %i private-key /etc/wireguard/privatekey";
+    #       peers = [
+    #         {
+    #           publicKey = "yPTvlsTZnzAxfn2GxrvSQX5/ymcsSFqSLtHiJ7zJITc=";
+    #           allowedIPs = ["10.0.0.0/24"];
+    #           endpoint = "nalgor.net:41883";
+    #           persistentKeepalive = 25;
+    #         }
+    #       ];
+    #     };
+    #   };
+    # };
   };
 
   system.stateVersion = "25.11";
