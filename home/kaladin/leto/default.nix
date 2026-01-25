875892f95d164392c6a505cfaff35eb64ed61c32
Igor Semyonov <igor@semyonov.xyz>
2026-01-25 01:37:48 -0500

    kaladin ready, probably

--- home/kaladin/leto/default.nix
+++ home/kaladin/leto/default.nix
@@ -3,10 +3,12 @@
   config,
   nhModules,
   userConfig,
+inputs,
   ...
 }: {
   imports = [
     "${nhModules}/programs/alacritty"
+    inputs.nix-flatpak.homeManagerModules.nix-flatpak
   ];
 
   # Enable home-manager
@@ -35,6 +37,16 @@
     "/var/lib/flatpak/exports/share"
     "${config.home.homeDirectory}/.local/share/flatpak/exports/share"
   ];
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    historyControl = ["erasedups" "ignoreboth"];
+    shellAliases = {
+      flatpak = "flatpak --user";
+      lo = "libreoffice";
+    };
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "25.11";
