1e7a561769f7c7b4bb7604e62268128e819aee50
Igor Semyonov <igor@semyonov.xyz>
2026-01-27 22:15:22 -0500

    added flake-parts and import-tree

--- flake.nix
+++ flake.nix
@@ -5,6 +5,9 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
     # Home manager
     home-manager = {
       url = "github:nix-community/home-manager";
