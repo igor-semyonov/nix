3d0bd90d2bd67f5a654946da42539459dab1bec4
Igor Semyonov <igor@semyonov.xyz>
2026-02-09 09:49:44 -0500

    added some cuda packages to nix-ld

--- modules/nixos/services/unpatched/default.nix
+++ modules/nixos/services/unpatched/default.nix
@@ -1,31 +1,44 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs = {
     nix-ld = {
       enable = true;
-      libraries = with pkgs; [
-        stdenv.cc.cc.lib
-        cudaPackages.cudatoolkit.lib
-        linuxPackages.nvidia_x11
+      libraries = with pkgs;
+        [
+          stdenv.cc.cc.lib
+          cudaPackages.cudatoolkit.lib
+          cudaPackages.cudnn
+          linuxPackages.nvidia_x11
+          zlib
+          glib
+          openssl
 
-        # matplotlib and pyside6
-        libGL
-        libxkbcommon
-        fontconfig
-        libx11
-        glib
-        freetype
-        dbus
-        kdePackages.wayland
-        kdePackages.qtwayland
-        xorg.xcbutil
-        xorg.xcbutilcursor
-        xorg.xcbutilwm
-        xorg.xcbutilkeysyms
-        xorg.xcbutilrenderutil
-        xorg.xcbutilimage
-        libxcb
-        libdrm
-      ];
+          # matplotlib and pyside6
+          libGL
+          libxkbcommon
+          fontconfig
+          libx11
+          freetype
+          dbus
+          kdePackages.wayland
+          kdePackages.qtwayland
+          xorg.xcbutil
+          xorg.xcbutilcursor
+          xorg.xcbutilwm
+          xorg.xcbutilkeysyms
+          xorg.xcbutilrenderutil
+          xorg.xcbutilimage
+          libxcb
+          libdrm
+        ]
+        ++ pkgs.pythonManylinuxPackages.manylinux1
+        ++ lib.optionals (pkgs.system == "x86_64-linux") (with pkgs.cudaPackages; [
+          libcutensor
+          nccl
+        ]);
     };
   };
 
