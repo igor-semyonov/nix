9447b25b360b0d702c052b5141cc97348bc843fc
Igor Semyonov <igor@semyonov.xyz>
2026-03-05 23:55:53 -0500

    switched u2f options for users and keys to single attrset mapping users to keys

--- modules/nixosModules/u2f.nix
+++ modules/nixosModules/u2f.nix
@@ -10,23 +10,23 @@ in {
   in {
     options.${ns}.u2f = {
       enable = lib.mkEnableOption "Enable u2f login using yubikey for the given users using the specified yubikey";
-      keys = lib.mkOption {
-        type = lib.types.listOf lib.types.singleLineStr;
-        default = [
-          "b4BNgldsZhyV6AqJe12pB9iQK91y2Cr5BV34v1yl59Qg5UJRxW7bnjQWuOc3Rk5Fgpumo4zSVPdcNoA7OJfgOQ==,GjTum3zXi2oCpA6UsHu2LlgFWn8n4ruV/fV2ZWku5W3QKkWyanjIbW9L6HL5U4ycKaagdwxrvT3Yt4fHaIriWQ==,es256,+presence" # main
-          "cohCmzUQUhBfal8rIjBdwRGp62TOhYmEw0OgS/D0XQzeCTxDaii/ftHwlkACXU3c1S17HdmdMFKBf5OYpnuOiw==,0Wbp+SrwTTlOU/2Ogq0tC2oh+emSYNPBcl3QznOyONqXSMDt6AudB0Dzq1uYRDRxj3++PUUeOxIONiAImGmh6g==,es256,+presence" # backup
-        ];
-        description = "List of the results of running `pamu2fcfg -o pam://<shared-id>` with the leading '<username>:' stripped. for each yubikey";
-      };
       sharedId = lib.mkOption {
         type = lib.types.singleLineStr;
         default = "pam://igix";
         description = "The shared id used to generate the u2f keys";
       };
-      users = lib.mkOption {
-        type = lib.types.listOf lib.types.singleLineStr;
-        default = ["igor"];
-        description = "List of users who will use these keys to sign in.";
+      userKeys = lib.mkOption {
+        description = "Attrset of users to list of keys for each user. The keys are generated using `pamu2fcfg -o pam://<sharedid>`";
+        type = lib.types.attrsWith {
+          elemType = lib.types.listOf lib.types.singleLineStr;
+          placeholder = "username";
+        };
+        default = {
+          igor = [
+            "b4BNgldsZhyV6AqJe12pB9iQK91y2Cr5BV34v1yl59Qg5UJRxW7bnjQWuOc3Rk5Fgpumo4zSVPdcNoA7OJfgOQ==,GjTum3zXi2oCpA6UsHu2LlgFWn8n4ruV/fV2ZWku5W3QKkWyanjIbW9L6HL5U4ycKaagdwxrvT3Yt4fHaIriWQ==,es256,+presence" # main
+            "cohCmzUQUhBfal8rIjBdwRGp62TOhYmEw0OgS/D0XQzeCTxDaii/ftHwlkACXU3c1S17HdmdMFKBf5OYpnuOiw==,0Wbp+SrwTTlOU/2Ogq0tC2oh+emSYNPBcl3QznOyONqXSMDt6AudB0Dzq1uYRDRxj3++PUUeOxIONiAImGmh6g==,es256,+presence" # backup
+          ];
+        };
       };
       authfile = lib.mkOption {
         type = lib.types.singleLineStr;
@@ -50,17 +50,14 @@ in {
         };
         services.sddm.u2fAuth = false;
       };
-      environment.etc.${cfg.authfile} = {
-        text =
-          lib.strings.join "\n"
-          (map (
-              user:
-                "${user}:"
-                + lib.strings.join ":"
-                cfg.keys
-            )
-            cfg.users);
-      };
+      environment.etc.${cfg.authfile}.text =
+        lib.strings.join "\n"
+        (
+          lib.mapAttrsToList (
+            user: keys: lib.strings.join ":" ([user] ++ keys)
+          )
+          cfg.userKeys
+        );
     };
   };
 }
