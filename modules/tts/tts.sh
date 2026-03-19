07b64aedb0c30121d2d5fd320cc5ba6417bfb2be
Igor Semyonov <igor@semyonov.xyz>
2026-03-19 13:12:12 -0400

    fixing tts pronunciation

--- modules/tts/tts.sh
+++ modules/tts/tts.sh
@@ -10,5 +10,6 @@ text=$(echo "$text" | iconv -f utf-8 -t ascii//translit)
 # text=$(echo "$text" | tr -d "<>")
 text=''${text//>/rangle}
 text=''${text//</langle}
+text=" ${text}" # Preppending a space fixes some pronunciation
 
 echo "$text" | wine 'C:\balcon\balcon.exe' -i -n 'Microsoft Server Speech Text to Speech Voice (en-US, ZiraPro)' -s "$tts_speed" &>/dev/null
