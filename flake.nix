7c7e918a39583316604f56781a84086a6432ee50
Igor Semyonov <igor@semyonov.xyz>
2026-03-24 16:24:00 -0400

    added option to specify this flake uses git lfs

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "NixOS and nix-darwin configs for my machines";
   inputs = {
+    self.lfs = true;
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
