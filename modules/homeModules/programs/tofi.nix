aae9eec1b3706f61b212cfdb5edacf3144b4b625
Igor Semyonov <igor@semyonov.xyz>
2026-03-28 22:07:39 -0400

    updated tofi look

--- modules/homeModules/programs/tofi.nix
+++ modules/homeModules/programs/tofi.nix
@@ -8,17 +8,17 @@
       enable = true;
       settings = {
         background-color = "#000000";
-        border-width = 0;
+        border-width = 20;
         font = "${pkgs.nerd-fonts.fira-code}/share/fonts/truetype/NerdFonts/FiraCode/FiraCodeNerdFontPropo-Retina.ttf";
         font-size = 64;
         hint-font = false;
         height = "100%";
         width = "100%";
         num-results = 5;
-        outline-width = 8;
+        outline-width = 0;
         padding-top = "15%";
-        padding-left = "20%";
-        result-spacing = 25;
+        padding-left = "15%";
+        result-spacing = 32;
 
         selection-color = "#80FF80";
         selection-background = "#606060";
