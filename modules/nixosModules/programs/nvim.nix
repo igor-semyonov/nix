1465134c6929d4e8f1a73f552f48e90db7b30f3a
Igor Semyonov <igor@semyonov.xyz>
2026-04-07 22:15:04 -0400

    switched nvim module to support specifying package

--- modules/nixosModules/programs/nvim.nix
+++ modules/nixosModules/programs/nvim.nix
@@ -1,13 +1,40 @@
-{inputs, ...}: {
-  flake.nixosModules.programs-nvim = {pkgs, ...}: {
-    environment = {
-      systemPackages = [
-        inputs.my-nvim.packages.${pkgs.stdenv.hostPlatform.system}.nvim-nixcats
-      ];
-      variables = {
-        EDITOR = "vim";
-        VISUAL = "vim";
-        SYSTEMD_EDITOR = "vim";
+{
+  config,
+  inputs,
+  ...
+}: let
+  ns = config.namespace-specifier;
+  variables = {
+    EDITOR = "vim";
+    VISUAL = "vim";
+    SYSTEMD_EDITOR = "vim";
+  };
+in {
+  flake.nixosModules.programs-nvim = {
+    pkgs,
+    lib,
+    config,
+    ...
+  }: let
+    cfg = config.${ns}.nvim;
+  in {
+    options.${ns}.nvim = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
+      package = lib.mkOption {
+        type = lib.types.package;
+        default = inputs.my-nvim.packages.${pkgs.stdenv.hostPlatform.system}.nvim-nixcats;
+        description = "The neovim package to be used";
+      };
+    };
+    config = lib.mkIf cfg.enable {
+      environment = {
+        inherit variables;
+        systemPackages = [
+          cfg.package
+        ];
       };
     };
   };
